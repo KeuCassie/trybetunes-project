768b5bb818921e33dc69f2fb2ae052de601d421b
Kelly Cassiano <keukssie@hotmail.com>
2022-02-03 17:09:58 -0300

    creating components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Components } from 'react';
+
+class Header extends Components {
+  render() {
+    return (
+      <header data-testid="header-component" />
+
+    );
+  }
+}
+
+export default Header;
